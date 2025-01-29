55fe72c8656271e6b8ac0230f2c88991f7bf4b5c
elfaroliver <eos35@hi.is>
2025-01-29 22:29:49 +0000

    Startaði Heimadæmi2

---
+++ Heimadæmi/Heimadæmi2/Mystery.java
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////
+// Beinagrind að lausn á dæmi 1 í Heimadæmum 2.
+// Tekur inn heiltöluna n á skipanalínunni og prentar
+// út nokkrar tölustafi.
+//
+// Keyrsla: $ java Mystery 20
+// 10100
+//
+// Hjálmtýr Hafsteinsson, Tölvunarfræði 2
+/////////////////////////////////////////////////////////////
+import edu.princeton.cs.algs4.*;
+public class Mystery {
+public static void main(String[] args) {
+int n = Integer.parseInt(args[0]);
+Stack<Integer> S = new Stack<Integer>();
+// Ykkar kóði hér ...
+}
+}
