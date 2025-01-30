977398f0f9277a3e3ea490fd59214be5e4e5630d
elfaroliver <eos35@hi.is>
2025-01-30 10:55:17 +0000

    settings.json uppfært með java path

--- Heimadæmi/Heimadæmi2/Mystery.java
+++ Heimadæmi/Heimadæmi2/Mystery.java
@@ -10,9 +10,9 @@
 /////////////////////////////////////////////////////////////
 import edu.princeton.cs.algs4.*;
 public class Mystery {
-public static void main(String[] args) {
-int n = Integer.parseInt(args[0]);
-Stack<Integer> S = new Stack<Integer>();
-// Ykkar kóði hér ...
-}
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        Stack<Integer> S = new Stack<Integer>();
+        // Ykkar kóði hér ...
+        }
 }
