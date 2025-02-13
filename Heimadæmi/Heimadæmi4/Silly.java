3267294fe326d122290f3d9345825cc317554e88
elfaroliver <eos35@hi.is>
2025-02-13 15:44:00 +0000

    Næstum búinn með Silly og Selection

--- Heimadæmi/Heimadæmi4/Silly.java
+++ Heimadæmi/Heimadæmi4/Silly.java
@@ -5,6 +5,7 @@
 *********************************************************************/
 import java.util.Comparator;
 import edu.princeton.cs.algs4.*;
+
 public class Silly {
     // This class should not be instantiated.
     private Silly() { }
@@ -17,41 +18,41 @@ public class Silly {
     ***************************************************************************/
     // is v < w ?
     private static boolean less(Comparable v, Comparable w) {
-    return v.compareTo(w) < 0;
+        return v.compareTo(w) < 0;
     }
     // exchange a[i] and a[j]
     private static void exch(Object[] a, int i, int j) {
-    Object swap = a[i];
-    a[i] = a[j];
-    a[j] = swap;
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
     }
     /***************************************************************************
     * Check if array is sorted - useful for debugging.
     ***************************************************************************/
     // is the array a[] sorted?
     private static boolean isSorted(Comparable[] a) {
-    return isSorted(a, 0, a.length - 1);
+        return isSorted(a, 0, a.length - 1);
     }
     // is the array sorted from a[lo] to a[hi]
     private static boolean isSorted(Comparable[] a, int lo, int hi) {
-    for (int i = lo + 1; i <= hi; i++)
-    if (less(a[i], a[i-1])) return false;
-    return true;
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
     }
     // print array to standard output
     private static void show(Comparable[] a) {
-    for (int i = 0; i < a.length; i++) {
-    StdOut.println(a[i]);
-    }
+        for (int i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
     }
     public static void main(String[] args) {
-    String[] a = StdIn.readAllStrings();
-    Stopwatch timer = new Stopwatch();
-    Silly.sort(a);
-    double eTime = timer.elapsedTime();
-    StdOut.println("elapsed time = " + eTime);
-    // Ef við viljum athuga hvort fylkið sé raðað
-    // show(a);
+        String[] a = StdIn.readAllStrings();
+        Stopwatch timer = new Stopwatch();
+        Silly.sort(a);
+        double eTime = timer.elapsedTime();
+        StdOut.println("elapsed time = " + eTime);
+        // Ef við viljum athuga hvort fylkið sé raðað
+        // show(a);
     }
 }
 
